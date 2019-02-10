286503fd1bb6b71556e73753641b8e1d9acdf604
Agnaldo Junior <agnaldo.junior@unoesc.edu.br>
2019-02-10 14:19:03 -0200

    Ajustando e testando a deleção de cliente

--- src/main/java/com/agnaldo/cursomc/services/ClienteService.java
+++ src/main/java/com/agnaldo/cursomc/services/ClienteService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.agnaldo.cursomc.domain.Cidade;
 import com.agnaldo.cursomc.domain.Cliente;
@@ -42,7 +43,8 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
-	
+	//Para garantir que será inserido o Cliente com o Endereço de forma transasional (para garantir que serão salvo tanto o cliente quanto o endereço na mesma transação)
+	@Transactional
 	public Cliente insert(Cliente obj)
 	{
 		obj.setId(null);
@@ -94,8 +96,9 @@ public class ClienteService {
 	public Cliente fromDTO(ClienteNewDTO objDto)
 	{
 		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
-		Optional<Cidade> cid = cidadeRepository.findById(objDto.getCidadeId());
-		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid.orElse(null));
+		//Optional<Cidade> cid = cidadeRepository.findById(objDto.getCidadeId());
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
 		cli.getEnderecos().add(end);
 		cli.getTelefones().add(objDto.getTelefone1());
 		if(objDto.getTelefone2()!=null)
