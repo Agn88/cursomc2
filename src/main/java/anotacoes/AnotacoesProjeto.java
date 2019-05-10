d86645fbd20eaae65c1897f968bb7718be0ff2e6
agn88 <agnaldo.junior@unoesc.edu.br>
2019-05-10 16:17:52 -0300

    Configuração e envio do projeto para produção

---
+++ src/main/java/anotacoes/AnotacoesProjeto.java
@@ -0,0 +1,63 @@
+package anotacoes;
+
+public class AnotacoesProjeto {
+
+	
+	
+	/*
+	 * HEROKU
+	 * 
+	 * 
+	 * Configurando o Heroku no projeto GIT:
+	 *  heroku git:remote -a curso-spring-ionic-agn88
+	 *  
+	 *  git remote -v
+	 *  
+	 *  
+	 *  
+	 *  
+	 *  
+	 *  
+	 *  
+	 *  
+	 * 
+	 * Enviando SQL: 
+	 * 
+	 * Comando digitado no GIT para gerar informacoes do banco que serão utilizadas para enviar o SQL para o Heroku:
+	 * $ heroku config | grep CLEARDB_DATABASE_URL
+	 * RETORNO:      CLEARDB_DATABASE_URL: mysql://bb04c58435af8e:0fa65ac2@us-cdbr-iron-east-02.cleardb.net/heroku_4ebec8ec2b1aafb?reconnect=true
+	 * 
+	 * Terminal Windows na pasta do arquivo.sql:
+	 * mysql --host=us-cdbr-iron-east-02.cleardb.net --user=bb04c58435af8e --password=0fa65ac2 --reconnect heroku_4ebec8ec2b1aafb < arquivo.sql
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 */
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
